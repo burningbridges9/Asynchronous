96f06fe3e271dc691f633def143d0c17a92e1524
burningbridges9 <talipov9@gmail.com>
2021-12-04 16:28:47 +0300

    Add project files.

---
+++ AsyncFibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncFibonacci
+{
+    class Program
+    {
+        static long FibAlgoRec(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+
+                return (FibAlgoRec(n - 1) + FibAlgoRec(n - 2));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Task.Run(() => FibAlgoRec(42)).ContinueWith((task) => Console.WriteLine($"Fib algo Result: {task.Result}"));
+
+            while (true)
+            {
+                Console.Write("-");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Async_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            var task = Task.Run(()=>
+            {
+                Console.WriteLine($"Task started: thrId{Thread.CurrentThread.ManagedThreadId}"); // starts in another thread
+                for (int i = 0; i < 280; i++)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(200);
+                }
+            });
+
+
+            Console.WriteLine($"Main thrId{Thread.CurrentThread.ManagedThreadId}");
+            Print("!");
+            task.Wait();
+            Console.ReadKey();
+        }
+
+        static void Print(string s)
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                Console.Write(s);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
